fb6d5759d8a45e218e2637abb74d5a898f98fb57
Soumojitshome2023 <soumojitshome2021@gmail.com>
2023-09-19 20:50:05 +0530

    icon add and update

--- script.js
+++ script.js
@@ -9,20 +9,6 @@ function use_add() {
     document.getElementById("use_price").value = "";
 
     let currentDate = new Date().toLocaleString();
-   
-
-    // let use = Number(document.querySelector("#total_use span").innerHTML);
-    // use = use + Number(use_price);
-    // document.querySelector("#total_use span").innerHTML = use;
-
-    // document.getElementById("tasks").innerHTML += `
-    //     <div class="card-body">
-    //         <h5 class="card-title topic_name_card">${use_topic}</h5>
-    //         <p class="card-text topic_price_card">${use_price}</p>
-    //         <a href="#" class="btn btn-primary">Delete</a>
-    //     </div>
-
-    // `
 
     document.getElementById("add_alert").style.display = "block";
     setTimeout(() => {
@@ -56,10 +42,10 @@ function load() {
 
             document.getElementById("tasks").innerHTML += `
             <div class="card-body">
-                <h5 class="card-title topic_name_card">${obj[ind][0]}</h5>
-                <p class="card-text topic_price_card">${obj[ind][1]}</p>
-                <p class="card-text topic_date_card">${obj[ind][2]}</p>
-                <button class="btn btn-primary" onclick="delete_use(${ind})">Delete</button>
+                <h5 class="card-title topic_name_card">Topic : ${obj[ind][0]}</h5>
+                <p class="card-text topic_price_card">Price : ${obj[ind][1]} INR</p>
+                <p class="card-text topic_date_card">Date : ${obj[ind][2]}</p>
+                <button class="btn btn-danger" onclick="delete_use(${ind})">Delete</button>
             </div>
         
             `
